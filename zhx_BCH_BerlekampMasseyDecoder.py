e309ffc2f9b17bcc0e87ea4b4ff29a29fca5a38c
ichinana-seu <atarashichiaki@hotmail.com>
2025-05-18 14:32:56 +0800

    v1

--- zhx_BCH_BerlekampMasseyDecoder.py
+++ zhx_BCH_BerlekampMasseyDecoder.py
@@ -1,9 +1,17 @@
+# version: 1 (2025-05-18)
+# 适用于 GF(2^m) 的Galois扩域。请注意：这里的基域只能是2。
+# 不可以是其他素数GF(p)->GF(p^m)或者GF(2^n)->GF(2^n^m)
+# 表示法：幂次表示法
+
 import numpy as np
 from GF2_map import GF2_map
 
+
+# 输入的应该是长度为2^m-1的 “-1或0”，输出也一样。如果要将幂级数表示法转换为010101二进制，则 【二进制=幂级数+1】 即可
 def zhx_BCH_BerlekampMasseyDecoder(received: np.ndarray, BCH_t: int, myGF2map: GF2_map):
     BCH_m = myGF2map.m
     BCH_n = 2**BCH_m -1
+    assert len(received) == BCH_n
     # 计算伴随式
     elements = np.arange(1, 2*BCH_t+1, dtype=np.int32)
     poly_function_value_parallel = np.vectorize(myGF2map.poly_function_value, excluded=[0])
@@ -12,9 +20,8 @@ def zhx_BCH_BerlekampMasseyDecoder(received: np.ndarray, BCH_t: int, myGF2map: G
     output = (-1) * np.ones(BCH_n, dtype=np.int32)
     failflag = 1
 
-
     # 特殊情况，通过校验无需译码
-    if np.all(syndrome == 0):
+    if np.all(syndrome == -1):
         output = received
         failflag = 0
         return output, failflag
@@ -31,42 +38,70 @@ def zhx_BCH_BerlekampMasseyDecoder(received: np.ndarray, BCH_t: int, myGF2map: G
     LminusK_stepdiff_itera[0] = -1
     # itera_k = 0
     itera_k = 0
-    discrepancy_itera[1] = syndrome[0]
+    discrepancy_itera[1] = syndrome[0]                                                                                                  # Ex: alpha^0
     sigma_function_itera[1, 0] = 0
     l_degreeOfSigmaFunc_itera[1] = 0
     LminusK_stepdiff_itera[1] = itera_k - l_degreeOfSigmaFunc_itera[1]          #   = 0
     # 预先计算下一个迭代的sigma_function
     max_stepdiff_indexi = -1
-    correction_term = myGF2map.mul( discrepancy_itera[0+1] , myGF2map.mulinverse( discrepancy_itera[max_stepdiff_indexi+1]   ) )
-    tmp = (-1) * np.ones( [0-max_stepdiff_indexi+1] , dtype=np.int32 )
-    tmp[0-max_stepdiff_indexi]
-    correction_term = myGF2map.mul( np.array(correction_term, dtype=np.int32) ,  tmp )
-    correction_term = myGF2map.mul( correction_term, myGF2map.poly_fresh( sigma_function_itera[max_stepdiff_indexi+1, :] )  )
+    correction_term = myGF2map.mul( discrepancy_itera[0+1] , myGF2map.mulinverse( discrepancy_itera[max_stepdiff_indexi+1]   ) )        # Ex: = symd0 = alpha^0
+    tmp = (-1) * np.ones( [itera_k-max_stepdiff_indexi+1] , dtype=np.int32 )
+    tmp[itera_k-max_stepdiff_indexi] = 0                                                                                                # Ex: [-1, 0]   = x^1
+    correction_term = myGF2map.mul( np.array(correction_term, dtype=np.int32) ,  tmp )                                                  # Ex: = [-1, symd0]            
+    correction_term = myGF2map.mul( correction_term, myGF2map.poly_fresh( sigma_function_itera[max_stepdiff_indexi+1, :] )  )           # Ex: = [-1, symd0] 
+    next_sigma_function = myGF2map.poly_fresh( myGF2map.add( sigma_function_itera[itera_k+1, :] ,   correction_term  ) )
+    sigma_function_itera[itera_k+2, 0:len(next_sigma_function)] = next_sigma_function                                                   # Ex: = [0, symd0]
 
-    # itera_k = 1
+    # itera_k = 1 ... 2t-1
     for itera_k in range(1, 2*BCH_t):
-        # select the most giant setdiff
-        max_stepdiff_indexi = np.argmax( LminusK_stepdiff_itera[0:]  ) - 1
-
-
-        previous_sigma_function
-        previous_sigma_function = myGF2map.poly_fresh( sigma_function_itera[itera_k, :] )
-        correction_term = 
-
-
-        sigma_function_itera[]
-        l_degreeOfSigmaFunc_itera[itera_k+1] = len(myGF2map.poly_fresh(sigma_function_itera)) - 1
-
+        l_degreeOfSigmaFunc_itera[itera_k+1] = len(myGF2map.poly_fresh(sigma_function_itera[itera_k+1, :])) - 1
+        LminusK_stepdiff_itera[itera_k+1] = itera_k - l_degreeOfSigmaFunc_itera[itera_k+1]
         # 计算d_k，为下一次迭代做准备
-        discrepancy_itera[itera_k+1] = syndrome[1] + sigma_function_itera[]
-
+        d_tmp = syndrome[itera_k]
+        for lower_index in range(1, l_degreeOfSigmaFunc_itera[itera_k+1]+1):
+            d_tmp = myGF2map.add(d_tmp,       myGF2map.mul( sigma_function_itera[itera_k+1, lower_index] , syndrome(itera_k-lower_index) )    )
+        discrepancy_itera[itera_k+1] = d_tmp
         # 预先计算下一个迭代的sigma_function
+        if d_tmp == -1: 
+            sigma_function_itera[itera_k+2, :] = sigma_function_itera[itera_k+1, :]
+        else:       # 搜寻第 -1 ～ k-1 步，选择第i步，满足d_i ~= alpha^(-1)，且 「步异」i−l_i最大
+            candidate_elements = discrepancy_itera[0:itera_k+1]
+            mask = ( candidate_elements != -1)
+            if np.any(mask):
+                dedicate_elements = candidate_elements[mask]
+                max_valid = np.max(dedicate_elements)
+                max_index = np.where(dedicate_elements == max_valid)[0]
+            else:
+                raise NotImplementedError("[ERROR] Cannot find max stepdiff index i which also !=alpha^(-1)")
+            max_stepdiff_indexi = max_index-1
+            correction_term = myGF2map.mul( discrepancy_itera[itera_k+1] , myGF2map.mulinverse( discrepancy_itera[max_stepdiff_indexi+1]   ) )        # Ex: = symd0 = alpha^0
+            tmp = (-1) * np.ones( [itera_k-max_stepdiff_indexi+1] , dtype=np.int32 )
+            tmp[itera_k-max_stepdiff_indexi] = 0                                                                                                # Ex: [-1, 0]   = x^1
+            correction_term = myGF2map.mul( np.array(correction_term, dtype=np.int32) ,  tmp )                                                  # Ex: = [-1, symd0]            
+            correction_term = myGF2map.mul( correction_term, myGF2map.poly_fresh( sigma_function_itera[max_stepdiff_indexi+1, :] )  )           # Ex: = [-1, symd0] 
+            next_sigma_function = myGF2map.poly_fresh( myGF2map.add( sigma_function_itera[itera_k+1, :] ,   correction_term  ) )
+            sigma_function_itera[itera_k+2, 0:len(next_sigma_function)] = next_sigma_function                                      
+    # itera_k = 2t 结束啦
+    sigma_function_final = sigma_function_itera[2*BCH_t+1,:]
 
+    # try to find roots of sigma_function and take its mul_recipral
+    errorlocation = []
+    for ele in range(0,2**BCH_m-1):
+        if -1 == myGF2map.poly_function_value(sigma_function_final, ele):
+            errorlocation.append( myGF2map.mulinverse(ele)  )
+    errorlocation_index = np.array(errorlocation, dtype=np.int32)
+    error_polynomial = (-1) * np.ones(BCH_n, dtype=np.int32)
+    error_polynomial[errorlocation_index] = 0
+    corrected_polynomial = myGF2map.poly_add(received, myGF2map.poly_addinverse(error_polynomial) )
 
+    # valid answer?
+    syndrome_new = poly_function_value_parallel(corrected_polynomial, elements)
+    if np.all(syndrome_new == -1):
+        failflag = 0
+    # return
+    return corrected_polynomial, failflag
 
-    # last: itera_k = 2t
-    sigma_function = sigma_function_itera[2*BCH_t+1]
-    pass
+            
 
 if __name__ == "__main__":
     primitive_polynomial = np.array([1,1,0,0,1], dtype=np.int32)
